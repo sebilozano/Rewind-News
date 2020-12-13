fd1a462d6f28272b5b23a2ee3dcc6113e1cf7f64
Sebi Lozano <sebastianlozano22@gmail.com>
2020-12-13 00:17:22 -0500

    rotating key

--- rewindnews/settings.py
+++ rewindnews/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mujiequ5l+)#dg0t^m(rwm1-9xwg@7g361(4sz4!4bb(b@81yg'
+SECRET_KEY = .env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
