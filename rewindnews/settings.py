9152753528a684059d9be49cea711aed2f35ebc1
Sebi Lozano <sebastianlozano22@gmail.com>
2020-12-13 14:45:53 -0500

    fixing this env nonsense

--- rewindnews/settings.py
+++ rewindnews/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 import sys
 import config
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
